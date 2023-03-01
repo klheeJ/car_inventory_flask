125b18d93d77afac6e3e7d7bc91ec5f4ad6c20ac
Klhee Jacobs <bangklhee@gmail.com>
2023-02-28 22:22:33 -0600

    updated

--- config.py
+++ config.py
@@ -9,7 +9,6 @@ class Config():
         using Environment variables where available.
         Otherwise, create the config variable if not done already.
     '''
-
     FLASK_APP = os.getenv('FLASK_APP')
     FLASK_ENV = os.getenv('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY')
